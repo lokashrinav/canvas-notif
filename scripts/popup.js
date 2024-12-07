a0e7d6d9086acf630765bf0cbd4656640e67cbb1
Shrinav Loka <lokashrinav@gmail.com>
2024-12-07 06:05:52 -0500

    done

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,6 +2,10 @@ document.getElementById('save').addEventListener('click', () => {
     const token = document.getElementById('token').value;
     chrome.storage.sync.set({ token }, () => {
       alert('Token saved successfully!');
+      chrome.storage.sync.get('token', (data) => {
+        alert(`Stored token: ${data.token}`);
+      });
     });
+    
   });
   
